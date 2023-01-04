bbaf3ff547f3432d439091a0ab190854a773b9b3
Yazeed al-Momani <yazeed.02.momani@outlook.com>
2023-01-04 13:22:04 +0300

    initialize react router

--- src/App.js
+++ src/App.js
@@ -1,5 +1,45 @@
+import {
+  createBrowserRouter,
+  createRoutesFromElements,
+  Route,
+  RouterProvider,
+  Outlet,
+} from "react-router-dom";
+
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route
+      path="/"
+      errorElement={<h1>Error</h1>}
+      element={
+        <>
+          <h1>Root</h1>
+          <Outlet />
+        </>
+      }>
+      <Route
+        index
+        element={<h1>Home</h1>}
+      />
+      <Route
+        path="/test"
+        element={
+          <>
+            <h1>Test</h1>
+            <Outlet />
+          </>
+        }>
+        <Route
+          path="/test/sec"
+          element={<h1>Test 2</h1>}
+        />
+      </Route>
+    </Route>
+  )
+);
+
 function App() {
-  return <h1>Hello World!</h1>;
+  return <RouterProvider router={router} />;
 }
 
 export default App;
