c207181ff9e68273d8fe8e9bc4678b3f3b239795
Yazeed al-Momani <yazeed.02.momani@outlook.com>
2023-01-07 23:19:42 +0300

    use english and arabic contexts in home helmet

--- src/helmets/DefaultHelmet.jsx
+++ src/helmets/DefaultHelmet.jsx
@@ -1,14 +1,24 @@
 import { useContext } from "react";
 import { Helmet } from "react-helmet-async";
+import ArabicContext from "../context/arabic-context";
+import EnglishContext from "../context/english-context";
 import LangContext from "../context/lang-context";
 
 function DefaultHelmet() {
   const langContext = useContext(LangContext);
+  const arabicContext = useContext(ArabicContext);
+  const englishContext = useContext(EnglishContext);
+
+  let titleTemplate;
+  if (langContext.lang === "en")
+    titleTemplate = englishContext.helmet.defaultTemplate;
+  if (langContext.lang === "ar")
+    titleTemplate = arabicContext.helmet.defaultTemplate;
 
   return (
     <Helmet
       htmlAttributes={{ lang: langContext.lang, dir: langContext.dir }}
-      titleTemplate="%s | Josor Steels"
+      titleTemplate={titleTemplate}
     />
   );
 }
