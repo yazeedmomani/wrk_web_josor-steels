7241cda15a3c8c80f8543b2b033655c4a26c2f07
Yazeed al-Momani <yazeed.02.momani@outlook.com>
2023-01-07 23:04:42 +0300

    use dynamic title

--- src/helmets/HomeHelmet.jsx
+++ src/helmets/HomeHelmet.jsx
@@ -1,10 +1,16 @@
 import { useContext } from "react";
 import { Helmet } from "react-helmet-async";
+import ArabicContext from "../context/arabic-context";
+import LangContext from "../context/lang-context";
 
 function HomeHelmet() {
+  const langContext = useContext(LangContext);
+  const arabicContext = useContext(ArabicContext);
+
   return (
     <Helmet>
-      <title>Home</title>
+      {langContext.lang === 'en' && <title>Home</title>}
+      {langContext.lang === 'ar' && <title>{arabicContext.helmet.homeTitle}</title>}
     </Helmet>
   );
 }
