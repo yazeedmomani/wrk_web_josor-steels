51fa48211a981736921f40bb1482cb230fb1206c
Yazeed al-Momani <yazeed.02.momani@outlook.com>
2023-01-07 13:40:20 +0300

    bug fix

--- src/index.js
+++ src/index.js
@@ -1,16 +1,19 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { HelmetProvider } from "react-helmet-async";
 
 import "./reset.css";
 import "./variables.css";
 import "./typography.css";
 import "./index.css";
 
-import App from './App';
+import App from "./App";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
+    <HelmetProvider>
+      <App />
+    </HelmetProvider>
   </React.StrictMode>
 );
