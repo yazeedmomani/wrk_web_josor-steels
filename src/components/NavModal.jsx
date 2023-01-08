202f4023d2de58ddf61169150d85788a546a4e95
Yazeed al-Momani <yazeed.02.momani@outlook.com>
2023-01-08 14:37:50 +0300

    create modal

--- src/components/NavModal.jsx
+++ src/components/NavModal.jsx
@@ -1,11 +1,29 @@
 import { createPortal } from "react-dom";
 
+import { Link } from "react-router-dom";
+
 import styles from "./NavModal.module.css";
 
 function NavModal() {
   return createPortal(
     <>
       <div className={styles.backdrop}></div>
+      <div className={styles['modal-container']}>
+        <ul className={styles.modal}>
+          <li className={styles.list}>
+            <Link>About us</Link>
+          </li>
+          <li className={styles.list}>
+            <Link>Projects</Link>
+          </li>
+          <li className={styles.list}>
+            <Link>Our facility</Link>
+          </li>
+          <li className={styles.list}>
+            <Link>Contact</Link>
+          </li>
+        </ul>
+      </div>
     </>,
     document.querySelector("#NavModal")
   );
