5a57e2c57d87b77d727497cc357bb0bd415bf777
Yazeed al-Momani <yazeed.02.momani@outlook.com>
2023-01-08 19:08:31 +0300

    create slider

---
+++ src/components/Slider.jsx
@@ -0,0 +1,35 @@
+import Carousel from "react-multi-carousel";
+import "react-multi-carousel/lib/styles.css";
+
+const responsive = {
+  superLargeDesktop: {
+    // the naming can be any, depends on you.
+    breakpoint: { max: 4000, min: 3000 },
+    items: 5,
+  },
+  desktop: {
+    breakpoint: { max: 3000, min: 1024 },
+    items: 4,
+  },
+  tablet: {
+    breakpoint: { max: 1024, min: 464 },
+    items: 3,
+  },
+  mobile: {
+    breakpoint: { max: 464, min: 0 },
+    items: 1.75,
+  },
+};
+
+function Slider(props) {
+  return (
+    <Carousel
+      responsive={responsive}
+      removeArrowOnDeviceType={["tablet", "mobile"]}
+      keyBoardControl="true">
+      {props.children}
+    </Carousel>
+  );
+}
+
+export default Slider;
--- src/pages/home/HomeServices.jsx
+++ src/pages/home/HomeServices.jsx
@@ -1,12 +1,21 @@
 import SectionSpan from "../../components/SectionSpan";
+import Slider from "../../components/Slider";
 
 import styles from "./HomeServices.module.css";
 
 function HomeServices() {
   return (
-    <section className={styles.section} id="HomeServices">
+    <section
+      className={styles.section}
+      id="HomeServices">
       <SectionSpan>Professional services</SectionSpan>
       <h2 className="h3">We take on a wide variety of projects</h2>
+      <Slider>
+        <div className={styles.test}></div>
+        <div className={styles.test}></div>
+        <div className={styles.test}></div>
+        <div className={styles.test}></div>
+      </Slider>
     </section>
   );
 }
