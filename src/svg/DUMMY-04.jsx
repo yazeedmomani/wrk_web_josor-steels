df3e0e957dc841b710722b2ccb8b0ee73d358ade
Yazeed al-Momani <yazeed.02.momani@outlook.com>
2023-01-09 16:07:37 +0300

    add dummy svgs

---
+++ src/svg/DUMMY-04.jsx
@@ -0,0 +1,18 @@
+function DeliveryIcon() {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width="56"
+      height="45.5"
+      viewBox="0 0 56 45.5">
+      <path
+        id="truck"
+        d="M56,28.5l-7-14H38.5v-7A3.51,3.51,0,0,0,35,4H3.5A3.51,3.51,0,0,0,0,7.5v28L3.5,39H7.938a7,7,0,1,0,12.124,0H39.44a7,7,0,1,0,12.12,0H56V28.5Zm-17.5,0V18h7.256l5.25,10.5Z"
+        transform="translate(0 -4)"
+        fill="#8c0303"
+      />
+    </svg>
+  );
+}
+
+export default DeliveryIcon;
