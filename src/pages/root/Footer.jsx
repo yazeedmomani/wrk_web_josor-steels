4bbc7c448af392c00ea4dd6544f018d90f4e986f
Yazeed al-Momani <yazeed.02.momani@outlook.com>
2023-01-11 23:45:29 +0300

    add contact info

--- src/pages/root/Footer.jsx
+++ src/pages/root/Footer.jsx
@@ -1,7 +1,7 @@
 import Logo from "../../svg/Logo";
 import LangChanger from "../../components/buttons/LangChanger";
 import PrimaryLink from "../../components/buttons/PrimaryLink";
-import SocialMediaIcon from "../../svg/SocialMediaIcon";
+import ContactIcon from "../../svg/ContactIcon";
 
 import styles from "./Footer.module.css";
 
@@ -11,18 +11,39 @@ function Footer() {
       <div className={styles.center}>
         <Logo isDark={true} />
         <div className={styles["icons-container"]}>
-          <SocialMediaIcon
+          <ContactIcon
             styles={styles.icon}
             type="linkedIn"
             isDark={true}
           />
-          <SocialMediaIcon
+          <ContactIcon
             styles={styles.icon}
             type="facebook"
             isDark={true}
           />
         </div>
-
+        <div className={styles["tel-container"]}>
+          <a href="tel:+962798697131">
+            <ContactIcon
+              type="phone"
+              isDark={true}
+              styles={styles["tel-icon"]}
+            />
+            <span className={styles["tel-text"]}>+962798697131</span>
+          </a>
+        </div>
+        <div className={styles["mail-container"]}>
+          <a href="mailto:yazeed.02.momani@outlook.com">
+            <ContactIcon
+              type="email"
+              isDark={true}
+              styles={styles["email-icon"]}
+            />
+            <span className={styles["email-text"]}>
+              yazeed.02.momani@outlook.com
+            </span>
+          </a>
+        </div>
       </div>
     </footer>
   );
--- src/svg/ContactIcon.jsx
+++ src/svg/ContactIcon.jsx
@@ -1,6 +1,6 @@
-import styles from "./SocialMediaIcon.module.css";
+import styles from "./ContactIcon.module.css";
 
-function SocialMediaIcon(props) {
+function ContactIcon(props) {
   const iconStyles = `${styles.icon} ${props.isDark === true && styles.dark} ${
     props.isAnimated === true && styles.animated
   } ${props.styles}`;
@@ -13,7 +13,7 @@ function SocialMediaIcon(props) {
         version="1.1"
         xmlns="http://www.w3.org/2000/svg"
         viewBox="0 0 32 32">
-        <title>facebook2</title>
+        <title>facebook</title>
         <path d="M29 0h-26c-1.65 0-3 1.35-3 3v26c0 1.65 1.35 3 3 3h13v-14h-4v-4h4v-2c0-3.306 2.694-6 6-6h4v4h-4c-1.1 0-2 0.9-2 2v2h6l-1 4h-5v14h9c1.65 0 3-1.35 3-3v-26c0-1.65-1.35-3-3-3z"></path>
       </svg>
     ),
@@ -28,9 +28,31 @@ function SocialMediaIcon(props) {
         <path d="M29 0h-26c-1.65 0-3 1.35-3 3v26c0 1.65 1.35 3 3 3h26c1.65 0 3-1.35 3-3v-26c0-1.65-1.35-3-3-3zM12 26h-4v-14h4v14zM10 10c-1.106 0-2-0.894-2-2s0.894-2 2-2c1.106 0 2 0.894 2 2s-0.894 2-2 2zM26 26h-4v-8c0-1.106-0.894-2-2-2s-2 0.894-2 2v8h-4v-14h4v2.481c0.825-1.131 2.087-2.481 3.5-2.481 2.488 0 4.5 2.238 4.5 5v9z"></path>
       </svg>
     ),
+    email: (
+      <svg
+        onClick={props.onClick}
+        className={iconStyles}
+        version="1.1"
+        xmlns="http://www.w3.org/2000/svg"
+        viewBox="0 0 32 32">
+        <title>e-mail</title>
+        <path d="M29 4h-26c-1.65 0-3 1.35-3 3v20c0 1.65 1.35 3 3 3h26c1.65 0 3-1.35 3-3v-20c0-1.65-1.35-3-3-3zM12.461 17.199l-8.461 6.59v-15.676l8.461 9.086zM5.512 8h20.976l-10.488 7.875-10.488-7.875zM12.79 17.553l3.21 3.447 3.21-3.447 6.58 8.447h-19.579l6.58-8.447zM19.539 17.199l8.461-9.086v15.676l-8.461-6.59z"></path>
+      </svg>
+    ),
+    phone: (
+      <svg
+        onClick={props.onClick}
+        className={iconStyles}
+        version="1.1"
+        xmlns="http://www.w3.org/2000/svg"
+        viewBox="0 0 32 32">
+        <title>phone</title>
+        <path d="M22 20c-2 2-2 4-4 4s-4-2-6-4-4-4-4-6 2-2 4-4-4-8-6-8-6 6-6 6c0 4 4.109 12.109 8 16s12 8 16 8c0 0 6-4 6-6s-6-8-8-6z"></path>
+      </svg>
+    ),
   };
 
   return icons[props.type];
 }
 
-export default SocialMediaIcon;
+export default ContactIcon;
