64939129ee6f7b686694bc274a888b8cda7dc8ff
Yazeed al-Momani <yazeed.02.momani@outlook.com>
2023-01-12 00:04:20 +0300

    change contact info info

--- src/components/general/ContactInfo.jsx
+++ src/components/general/ContactInfo.jsx
@@ -5,20 +5,20 @@ import styles from "./ContactInfo.module.css";
 function ContactInfo(props) {
   return (
     <div className={`${styles.container} ${props.styles}`}>
-      <a href="tel:+962798697131">
+      <a href="tel:+962791621064">
         <ContactIcon
           type="phone"
           isDark={true}
           styles={styles["tel-icon"]}
         />
-        <span>+962798697131</span>
+        <span>+962791621064</span>
       </a>
-      <a href="mailto:yazeed.02.momani@outlook.com">
+      <a href="mailto:example@fabricators.com">
         <ContactIcon
           type="email"
           isDark={true}
         />
-        <span>yazeed.02.momani@outlook.com</span>
+        <span>example@fabricators.com</span>
       </a>
     </div>
   );
