e984a2969d14fb28470c03d7763011656cfda403
Yazeed al-Momani <yazeed.02.momani@outlook.com>
2023-01-13 00:27:08 +0300

    create projects page

--- src/pages/projects/Projects.jsx
+++ src/pages/projects/Projects.jsx
@@ -1,10 +1,39 @@
+import { useContext } from "react";
+import { Link } from "react-router-dom";
+
 import DynamicHelmet from "../../helmets/DynamicHelmet";
+import Image from "../../components/general/Image";
+
+import styles from "./Projects.module.css";
+
+import LangContext from "../../contexts/lang-context";
+import ContentContext from "../../contexts/content-context";
+import images from "../../contents/images";
 
 function Projects() {
+  const langContext = useContext(LangContext);
+  const contentContext = useContext(ContentContext);
+  //TODO add langContext below
+  const projectsContext = contentContext["en"].projects.categoryCards;
+  const projectImages = images.projectCategories;
+
   return (
     <>
       <DynamicHelmet page="projects" />
-      <h1>Projects</h1>
+      <div className={styles.center}>
+        <h1 className="h2">Projects</h1>
+        {projectsContext.map((cur, i) => (
+          <Link
+            to={cur.to}
+            key={i}>
+            <Image
+              src={projectImages[cur.image]}
+              alt={cur.alt}
+              title={cur.title}
+            />
+          </Link>
+        ))}
+      </div>
     </>
   );
 }
