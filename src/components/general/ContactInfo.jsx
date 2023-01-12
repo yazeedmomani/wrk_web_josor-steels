a777d7365adffcce507ad50eb8ab934d95631ec2
Yazeed al-Momani <yazeed.02.momani@outlook.com>
2023-01-12 11:37:14 +0300

    make contact info component content dynamic

--- src/components/general/ContactInfo.jsx
+++ src/components/general/ContactInfo.jsx
@@ -1,24 +1,34 @@
+import { useContext } from "react";
+
 import ContactIcon from "../../svg/ContactIcon";
 
 import styles from "./ContactInfo.module.css";
 
+import LangContext from "../../contexts/lang-context";
+import ContentContext from "../../contexts/content-context";
+
 function ContactInfo(props) {
+  const langContext = useContext(LangContext);
+  const contentContext = useContext(ContentContext);
+  const componentContext =
+    contentContext[langContext.lang].components.ContactInfo;
+
   return (
     <div className={`${styles.container} ${props.styles}`}>
-      <a href="tel:+962791621064">
+      <a href={`tel:${componentContext.phone}`}>
         <ContactIcon
           type="phone"
           isDark={true}
           styles={styles["tel-icon"]}
         />
-        <span>+962791621064</span>
+        <span>{componentContext.phone}</span>
       </a>
-      <a href="mailto:example@fabricators.com">
+      <a href={`mailto:${componentContext.email}`}>
         <ContactIcon
           type="email"
           isDark={true}
         />
-        <span>example@fabricators.com</span>
+        <span>{componentContext.email}</span>
       </a>
     </div>
   );
