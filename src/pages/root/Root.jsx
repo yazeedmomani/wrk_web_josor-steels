c197b1adce08bbb19ba63715b13380f47d9bdb35
Yazeed al-Momani <yazeed.02.momani@outlook.com>
2023-01-13 00:38:00 +0300

    bug fix

--- src/pages/root/Root.jsx
+++ src/pages/root/Root.jsx
@@ -3,10 +3,12 @@ import { Outlet } from "react-router-dom";
 import RootHelmet from "../../helmets/RootHelmet";
 import Nav from "./Nav";
 import Footer from "./Footer";
+import ScrollToTop from "../../components/fixes/ScrollToTop";
 
 function Root() {
   return (
     <>
+      <ScrollToTop />
       <RootHelmet />
       <Nav />
       <main>
