ddf57a69310ba5840859dc54ac129118fe26c614
Yazeed al-Momani <yazeed.02.momani@outlook.com>
2023-01-14 01:54:42 +0300

    change paths to match deployment

--- src/contents/link-paths.js
+++ src/contents/link-paths.js
@@ -1,18 +1,18 @@
 const path = {
-  home: "/",
-  aboutUs: "/about-us",
+  home: "/josor-steels/",
+  aboutUs: "/josor-steels/about-us",
   projects: {
-    path: "/projects",
+    path: "/josor-steels/projects",
     children: {
-      hangers: "/projects/hangers",
-      bridges: "/projects/bridges",
-      tubes: "/projects/tubes",
-      canopies: "/projects/canopies",
-      other: "/projects/other",
+      hangers: "/josor-steels/projects/hangers",
+      bridges: "/josor-steels/projects/bridges",
+      tubes: "/josor-steels/projects/tubes",
+      canopies: "/josor-steels/projects/canopies",
+      other: "/josor-steels/projects/other",
     },
   },
-  ourFacility: "/our-facility",
-  contact: "/contact",
+  ourFacility: "/josor-steels/our-facility",
+  contact: "/josor-steels/contact",
 };
 
 export default path;
