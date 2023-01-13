8cc3bb1859c1d9a905bc23d5d1e3140ee0d8b31a
Yazeed al-Momani <yazeed.02.momani@outlook.com>
2023-01-13 17:38:42 +0300

    add onClick prop to image component

--- src/components/general/Image.jsx
+++ src/components/general/Image.jsx
@@ -2,19 +2,23 @@ import styles from "./Image.module.css";
 
 function Image(props) {
   return (
-    <div className={`${styles.container} ${props.styles}`}>
+    <div
+      className={`${styles.container} ${props.styles}`}
+      onClick={props.onClick}>
       <img
         src={props.src}
         alt={props.alt}
         className={styles.img}
       />
       <div className={styles.gradient}></div>
-      {props.title && <span
-        className="h3"
-        role="heading"
-        aria-level="3">
-        {props.title}
-      </span>}
+      {props.title && (
+        <span
+          className="h3"
+          role="heading"
+          aria-level="3">
+          {props.title}
+        </span>
+      )}
     </div>
   );
 }
