bad19e49d6e84ae40048011f457879439646793a
Yazeed al-Momani <yazeed.02.momani@outlook.com>
2023-01-13 17:35:42 +0300

    create our facility

--- src/pages/OurFacility.jsx
+++ src/pages/OurFacility.jsx
@@ -2,15 +2,19 @@ import { useContext } from "react";
 
 import DynamicHelmet from "../helmets/DynamicHelmet";
 import PathNav from "../components/general/PathNav";
+import PaginatedItems from "../components/pagination/PaginatedItems";
+import Image from "../components/general/Image";
 
 import styles from "./OurFacility.module.css";
 
 import LangContext from "../contexts/lang-context";
 import ContentContext from "../contexts/content-context";
+import images from "../contents/images";
 
 function OurFacility() {
   const langContext = useContext(LangContext);
   const contentContext = useContext(ContentContext);
+  const imagesContext = images.ourFacility;
   const pathNavContext =
     contentContext[langContext.lang].components.PathNav.ourFacility;
 
@@ -22,6 +26,15 @@ function OurFacility() {
           items={pathNavContext}
           styles={styles.path}
         />
+        <h1 className="h2">Our facility</h1>
+        <PaginatedItems
+          itemsPerPage={5}
+          controlsClassName={styles.controls}
+          itemClassName={styles.item}>
+          {imagesContext.map((cur) => (
+            <Image src={cur} />
+          ))}
+        </PaginatedItems>
       </div>
     </>
   );
