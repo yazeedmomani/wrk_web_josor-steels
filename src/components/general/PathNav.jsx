62348772582e83df47e4205a32a49e11e0bfccf5
Yazeed al-Momani <yazeed.02.momani@outlook.com>
2023-02-08 14:40:00 +0300

    bug fix

--- src/components/general/PathNav.jsx
+++ src/components/general/PathNav.jsx
@@ -11,21 +11,25 @@ function PathNav(props) {
 
   return (
     <div className={`${styles.container} ${props.styles}`}>
-      {props.items &&
-        props.items.map((cur, i) =>
-          cur.isLink ? (
-            <>
-              <PrimaryLink
-                styles={styles.link}
-                to={cur.to}>
-                {cur.text}
-              </PrimaryLink>
-              <Arrow isLeft={langContext.dir === "rtl" ? true : false} />
-            </>
-          ) : (
-            <span>{cur.text}</span>
-          )
-        )}
+      <ul className={styles.ul}>
+        {props.items &&
+          props.items.map((cur, i) =>
+            cur.isLink ? (
+              <li key={i} className={styles.li}>
+                <PrimaryLink
+                  styles={styles.link}
+                  to={cur.to}>
+                  {cur.text}
+                </PrimaryLink>
+                <Arrow isLeft={langContext.dir === "rtl" ? true : false} />
+              </li>
+            ) : (
+              <li key={i}>
+                <span>{cur.text}</span>
+              </li>
+            )
+          )}
+      </ul>
     </div>
   );
 }
