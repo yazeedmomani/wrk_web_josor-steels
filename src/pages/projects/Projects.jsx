a73b68375313d5afcb90c24369c074f1db372cd3
Yazeed al-Momani <yazeed.02.momani@outlook.com>
2023-02-08 14:32:59 +0300

    make projects page responsive

--- src/pages/projects/Projects.jsx
+++ src/pages/projects/Projects.jsx
@@ -1,5 +1,7 @@
-import { useContext } from "react";
+import { useContext, useEffect, useState } from "react";
 import { Link } from "react-router-dom";
+
+import useWindowDimensions from "../../hooks/useWindowDimensions";
 
 import DynamicHelmet from "../../helmets/DynamicHelmet";
 import Image from "../../components/general/Image";
@@ -10,14 +12,24 @@ import styles from "./Projects.module.css";
 import LangContext from "../../contexts/lang-context";
 import ContentContext from "../../contexts/content-context";
 import images from "../../contents/images";
+import Slider from "../../components/slider/Slider";
 
 function Projects() {
+  const [isDesktop, setIsDesktop] = useState(false);
+
   const langContext = useContext(LangContext);
   const contentContext = useContext(ContentContext);
   const projectsContext = contentContext[langContext.lang].projects;
   const projectImages = images.projectCategories;
   const pathNavContext =
     contentContext[langContext.lang].components.PathNav.projects.categoryPage;
+
+  const [winWidth] = useWindowDimensions();
+
+  useEffect(() => {
+    if (winWidth > 1024) setIsDesktop(true);
+    else setIsDesktop(false);
+  }, [winWidth]);
 
   return (
     <>
@@ -28,19 +40,41 @@ function Projects() {
           styles={styles.path}
         />
         <h1 className="h2">{projectsContext.title}</h1>
-        {projectsContext.categoryCards.map((cur, i) => (
-          <Link
-            className={styles["category-card"]}
-            to={cur.to}
-            key={i}>
-            <Image
-              src={projectImages[cur.image]}
-              alt={cur.alt}
-              title={cur.title}
-            />
-          </Link>
-        ))}
+        {!isDesktop && (
+          <div className={styles.flex}>
+            {projectsContext.categoryCards.map((cur, i) => (
+              <Link
+                className={styles["category-card"]}
+                to={cur.to}
+                key={i}>
+                <Image
+                  src={projectImages[cur.image]}
+                  alt={cur.alt}
+                  title={cur.title}
+                />
+              </Link>
+            ))}
+          </div>
+        )}
       </div>
+      {isDesktop && (
+        <Slider
+          autoPlay={true}
+          containerClass={styles.container}
+          dotListClass={styles.dots}>
+          {projectsContext.categoryCards.map((cur, i) => (
+            <Link
+              to={cur.to}
+              key={cur.title}>
+              <Image
+                src={projectImages[cur.image]}
+                alt={cur.alt}
+                title={cur.title}
+              />
+            </Link>
+          ))}
+        </Slider>
+      )}
     </>
   );
 }
