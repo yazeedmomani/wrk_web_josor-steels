446007daf68a0a3097204c056e9027ee8696eef8
Yazeed al-Momani <yazeed.02.momani@outlook.com>
2023-03-10 14:46:10 +0300

    further optimize backdrop

--- src/components/backdrop/backdrop.jsx
+++ src/components/backdrop/backdrop.jsx
@@ -1,13 +1,20 @@
+// Import necessary modules and components
 import { useMemo, memo, forwardRef } from "react";
-import classes  from "./backdrop.module.scss";
+import classes from "./backdrop.module.scss";
 
+// Destructure the classes object to access the "backdrop" CSS class
+const { backdrop } = classes;
+
+// Define the Backdrop component using memoization and forwarding of refs
 export default memo(
   forwardRef(function Backdrop({ onClick, children, className }, ref) {
+    // Create the container class string by joining the "backdrop" class and the passed-in className
     const divClasses = useMemo(
-      () => [classes.backdrop, className].join(" "),
+      () => [backdrop, className].join(" "),
       [className]
     );
 
+    // Render the Backdrop component, which is a clickable div that displays its children
     return (
       <div
         ref={ref}
