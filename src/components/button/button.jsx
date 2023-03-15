f63c4911ec7fc5023bb40000f5e21c8e103824af
Yazeed al-Momani <yazeed.02.momani@outlook.com>
2023-03-15 16:14:18 +0300

    optimize button component

--- src/components/button/button.jsx
+++ src/components/button/button.jsx
@@ -1,18 +1,25 @@
+// Importing the CSS classes for the button
 import classes from "./button.module.scss";
+
+// Importing a library for conditionally joining classnames
 import cn from "classnames";
 
+// Setting default props for the component
 Button.defaultProps = {
   type: "primary",
   href: "",
 };
 
+// Declaring the functional component
 export default function Button({ type, href, children, className, ...props }) {
+  // Conditionally joining classnames based on props
   const buttonClasses = cn(
     classes.button,
     classes[`button___${type}`],
     className
   );
 
+  // Rendering the button with a link
   return (
     <a
       href={href}
