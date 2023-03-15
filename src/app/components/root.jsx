6cb1df33a2d88e2dc6eb24500cabbc6103012b56
Yazeed al-Momani <yazeed.02.momani@outlook.com>
2023-03-15 16:35:05 +0300

    optimize root component

--- src/app/components/root.jsx
+++ src/app/components/root.jsx
@@ -1,24 +1,44 @@
-import { Outlet, useLocation } from "react-router-dom";
-
-import RootHelmet from "../../components/helmet/root";
+// Import required modules and components
 import Nav from "../../layouts/nav";
 import Footer from "../../layouts/footer";
+import { Outlet, useLocation } from "react-router-dom";
+import { memo, useCallback } from "react";
+import RootHelmet from "../../components/helmet/root";
 import ScrollToTop from "../../helpers/scroll-to-top";
 
-function Root() {
-  const { pathname } = useLocation();
-
+// Define a functional component for the basic layout of the website
+function Layout({ children }) {
   return (
     <>
-      <RootHelmet />
-      <ScrollToTop dependency={pathname} />
+      {/* Render navigation bar */}
       <Nav />
-      <main>
-        <Outlet />
-      </main>
+      {/* Render main content area */}
+      <main>{children}</main>
+      {/* Render footer */}
       <Footer />
     </>
   );
 }
 
-export default Root;
+// Create a memoized version of the Layout component
+const MemoizedLayout = memo(Layout);
+
+// Define the Root component, which is the main entry point for the website
+export default function Root() {
+  const { pathname } = useLocation(); // Get current pathname using the useLocation hook
+  const memoizedPathname = useCallback(() => pathname, [pathname]); // Create memoized function to return pathname
+
+  return (
+    <>
+      {/* Render RootHelmet component to set document title and meta tags*/}
+      <RootHelmet />
+      {/* Render ScrollToTop component to scroll to top on pathname change */}
+      <ScrollToTop dependency={memoizedPathname} />
+      {/* Render MemoizedLayout component, which includes navigation, main content, and footer*/}
+      <MemoizedLayout>
+        {/* Render child component corresponding to the current route */}
+        <Outlet />
+      </MemoizedLayout>
+    </>
+  );
+}
