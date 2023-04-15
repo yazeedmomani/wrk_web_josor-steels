a0364b63bc6bab60ef6fe427f22558a0b2aacb55
yazeedmomani <yazeed.02.momani@outlook.com>
2023-04-15 17:09:31 +0300

    add more content

--- src/contexts/content/contents/english.js
+++ src/contexts/content/contents/english.js
@@ -522,12 +522,13 @@ const english = {
   },
   components: {
     ContactInfo: {
-      phone: "+962791621064",
+      phone: "+962797141950",
       email: "example@fabricators.com",
     },
     ContactIcon: {
       facebook: "https://www.facebook.com",
-      linkedIn: "https://www.linkedIn.com",
+      linkedIn:
+        "https://www.linkedin.com/company/josor-steel-fabricators-%D8%B4%D8%B1%D9%83%D8%A9-%D8%A7%D9%84%D8%AC%D8%B3%D9%88%D8%B1-%D9%84%D9%84%D8%A3%D8%B9%D9%85%D8%A7%D9%84-%D8%A7%D9%84%D9%85%D8%B9%D8%AF%D9%86%D9%8A%D8%A9/about/",
     },
     ProjectCard: {
       button: "View images →",
